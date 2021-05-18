1095835561a68ea79e0eef5fc1e1090a1b9d263d
Zach Pate <>
2021-05-17 22:21:31 -0400

    Validate country of IP and respond - still need error handling and cleanup

--- main.go
+++ main.go
@@ -16,10 +16,16 @@ type RequestBody struct {
 	ValidCountries []string
 }
 
+type Response struct { 
+	IsCountryValid bool `json:"isCountryValid"`
+ 	ErrorString string `json:"errorString"`
+}
+
 var countryReader *geoip2.CountryReader
+const dbPath = "testdata/GeoLite2-Country.mmdb"
 
 func init() { 
-	reader, err := geoip2.NewCountryReaderFromFile("testdata/GeoLite2-Country.mmdb")
+	reader, err := geoip2.NewCountryReaderFromFile(dbPath)
 	if err != nil { 
 		panic(err)
 	}
@@ -28,11 +34,11 @@ func init() {
 }
 
 func handleRequests() { 
-	http.HandleFunc("/validateIpAddress", validateIpAddress)
+	http.HandleFunc("/validateIpAddress", handleValidateIpAddress)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
-func validateIpAddress(w http.ResponseWriter, r *http.Request) { 
+func handleValidateIpAddress(w http.ResponseWriter, r *http.Request) { 
 	fmt.Println("Received request at validateIpAddress")
 	decoder := json.NewDecoder(r.Body)
     var requestBody RequestBody
@@ -42,19 +48,46 @@ func validateIpAddress(w http.ResponseWriter, r *http.Request) {
     }
 
 	countryName := getCountryNameForIpAddress(requestBody.IpAddress)
+	isCountryValid := contains(requestBody.ValidCountries, countryName)
 
-    fmt.Println("IP ADDRESS:", requestBody.IpAddress)
-    fmt.Println("Valid Countries:", requestBody.ValidCountries)	
-	fmt.Println("COUNTRY:", countryName)
+	if isCountryValid { 
+		fmt.Println("Country Name is Valid")
+	} 
+
+	response := Response{isCountryValid, ""}
+	responseJson, err := json.Marshal(response)
+	if err != nil { 
+		panic(err)
+	}
+
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(responseJson)
+	// fmt.Println("Response", response)
 }
 
 func getCountryNameForIpAddress(ipAddress string) string { 
-	record, err := countryReader.Lookup(net.ParseIP(ipAddress))
-	countryName := record.Country.Names["en"]
+	parsedIp := net.ParseIP(ipAddress)
+	if parsedIp == nil {
+		panic("err")
+	}
+	record, err := countryReader.Lookup(parsedIp)
 	if err != nil {
 		panic(err)
 	}
+	countryName := record.Country.Names["en"]
 	return countryName
+}
+
+// Utility function to return whether a value exists in a slice
+func contains(s []string, str string) bool { 
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
 }
 
 func main() { 
