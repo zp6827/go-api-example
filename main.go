7b9c235d9427fcbd5753c7dc26da693eb2371631
Zach Pate <>
2021-05-17 20:51:10 -0400

    Extract JSON from request body

--- main.go
+++ main.go
@@ -4,9 +4,15 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"io/ioutil"
+	"encoding/json"
+	// "io/ioutil"
 	// "errors"
 )
+
+type RequestBody struct { 
+	IpAddress string
+	ValidCountries []string
+}
 
 func testPage(w http.ResponseWriter, r *http.Request) { 
 	fmt.Fprintf(w, "Validating IP Address...")
@@ -20,8 +26,15 @@ func handleRequests() {
 
 func validateIpAddress(w http.ResponseWriter, r *http.Request) { 
 	fmt.Println("Received request at validateIpAddress")
-	reqBody, _ := ioutil.ReadAll(r.Body)
-	fmt.Println(string(reqBody))
+	decoder := json.NewDecoder(r.Body)
+    var requestBody RequestBody
+    err := decoder.Decode(&requestBody)
+    if err != nil {
+        panic(err)
+    }
+
+    fmt.Println("IP ADDRESS:", requestBody.IpAddress)
+    fmt.Println("Valid Countries:", requestBody.ValidCountries)	
 }
 
 func main() { 
